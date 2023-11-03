0398ad0303b74a173eb235929a55df468f76e786
Anders0011 <122918296+Anders0011@users.noreply.github.com>
2023-11-03 18:13:01 -0400

    debut

---
+++ Affichage.hpp
@@ -0,0 +1,21 @@
+#include <iostream>  
+using namespace std;
+
+
+class Affichable 
+{ 
+public:  
+	virtual void afficher() = 0;
+	virtual void changerCouleur() = 0;  
+	Affichable operator<<() 
+	
+
+private: 
+
+
+
+
+
+
+}; 
+
